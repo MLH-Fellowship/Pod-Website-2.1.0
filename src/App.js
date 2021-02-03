865e4d9840d2bd32e9ab3ff2e37fa356d861d2bf
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-02-03 21:39:51 +0530

    Added boilerplate code

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+import Hero from "./containers/Hero/Hero";
+import Fellows from "./containers/Fellows/Fellows";
+import Projects from "./containers/Projects/Projects";
+
+const App = () => {
+  return (
+    <>
+      <Hero />
+      <Fellows />
+      <Projects />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/FellowCard/FellowCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./FellowCard.module.css";
+
+const FellowCard = () => {
+  return <div></div>;
+};
+
+export default FellowCard;
