17f6f77dd9964f462cc61fc19979470ffeb67d15
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-02-04 21:32:48 +0530

    Modify hero according to figma design to span full width and height

--- src/containers/Hero/Hero.js
+++ src/containers/Hero/Hero.js
@@ -7,12 +7,16 @@ import minionJump from '../../assets/images/minion-jump.gif';
 const Hero = () => {
   return (
     <div className={styles.container}>
-      <div>
+      <div className={styles.heroText}>
         <h1 className={styles.title}>HELLO! THERE</h1>
         <h2 className={styles.subtitle}>FELLOW EXPLORER!</h2>
       </div>
       <div>
-        <img src={minionJump} alt="Minions going Bananas!" />
+        <img
+          className={styles.gif}
+          src={minionJump}
+          alt="Minions going Bananas!"
+        />
       </div>
     </div>
   );
