3702c7f76c1f9e73f2d7ab2bea04ec24b48454b3
n-maido <nhi_maido@outlook.com>
2021-02-04 12:39:42 -0800

    add more fellow info to JSON array

--- src/containers/Fellows/constants.js
+++ src/containers/Fellows/constants.js
@@ -5,7 +5,7 @@ const fellows = [
         "github": "https://github.com/dev2919",
         "linkedin": "https://www.linkedin.com/in/devesh-pawar-60304113b/",
         "website": "https://www.devesh.design/",
-        "headshot": "https://drive.google.com/open?id=1MDD2RnGy4qqmv1f7AnMeYu3vQtdn6u_7",
+        "headshot": "https://drive.google.com/open?id=1MDD2RnGy4qqmv1f7AnMeYu3vQtdn6u_7"
     },
     {
         "name": "Keshavaa",
@@ -13,7 +13,23 @@ const fellows = [
         "github": "https://github.com/skeshavaa",
         "linkedin": "https://www.linkedin.com/in/keshavaa/",
         "website": "https://skeshavaa.live/",
-        "headshot": "https://drive.google.com/open?id=1TGxR4llNr8okrqMJauw_WqEmYaAM_CUq",
+        "headshot": "https://drive.google.com/open?id=1TGxR4llNr8okrqMJauw_WqEmYaAM_CUq"
+    },
+    {
+        "name": "Abhishek",
+        "bio": "An open source enthusiast having desire to grow",
+        "github": "https://github.com/Abhishek-kumar09",
+        "linkedin": "https://www.linkedin.com/in/abhishek-kumar-016ba1175/",
+        "website": " ",
+        "headshot": "https://drive.google.com/open?id=1lNVsSvV85musm797eYjmZzNI2H2eTRKP"
+    },
+    {
+        "name": "Nhi",
+        "bio": "Aspiring software developer from Vancouver, Canada. In my spare time, I enjoy longboarding and combat sports.",
+        "github": "https://github.com/n-maido",
+        "linkedin": "https://www.linkedin.com/in/nhimaido/",
+        "website": " ",
+        "headshot": "https://drive.google.com/open?id=15OH04-EwWMrOLq4hWIZysq499if_flVB"
     }
 ]
 
