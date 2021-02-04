5c2d83e1967d83677f688ab63f1fb8400d5293dd
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-02-05 02:24:58 +0530

    Updated dynamic tech icons

--- src/containers/Projects/components/TechTags.js
+++ src/containers/Projects/components/TechTags.js
@@ -1,11 +1,14 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import './CarouselCard.css'
 
-function TechTags() {
+function TechTags({ language, platform }) {
+    let plogo = "https://img.shields.io/badge/Made_For-" + platform + "-red?style=for-the-badge&logo=" + platform
+    let llogo = "https://img.shields.io/badge/Made_With-" + language + "-purple?style=for-the-badge&logo=" + language
     return (
         <div className="tagstech">
-            <img src="https://img.shields.io/badge/Made_With-Jekyll-purple?style=for-the-badge&logo=ruby" alt="" />
-            <img src="https://img.shields.io/badge/Made_For-Web-red?style=for-the-badge&logo=web" alt="" />
+            <img src={llogo} alt="" />
+            <img src={plogo} alt="" />
         </div>
     )
 }
