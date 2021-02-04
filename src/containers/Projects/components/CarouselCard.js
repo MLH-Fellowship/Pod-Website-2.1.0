f36a9317e66011ad1e89d31e89b766e16e12c230
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-02-05 02:06:22 +0530

    Updated icons and stuff

--- src/containers/Projects/components/CarouselCard.js
+++ src/containers/Projects/components/CarouselCard.js
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import './CarouselCard.css'
+import picture from '../../../assets/images/icons8-github-100.svg';
+import TechTags from './TechTags';
 
 function CarouselCard(props) {
     return (
         <div className="carouselCard">
             <h1 >{props.title}</h1>
-                <p>{props.techstack}</p>
+            <p>{props.techstack}</p>
+            <TechTags />
+            <a href="www.google.com"><img src={picture} /></a>
         </div>
     )
 }
---
+++ src/containers/Projects/components/TechTags.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CarouselCard.css'
+
+function TechTags() {
+    return (
+        <div className="tagstech">
+            <img src="https://img.shields.io/badge/Made_With-Jekyll-purple?style=for-the-badge&logo=ruby" alt="" />
+            <img src="https://img.shields.io/badge/Made_For-Web-red?style=for-the-badge&logo=web" alt="" />
+        </div>
+    )
+}
+
+export default TechTags
