1c2bcba17442067f994e63594805e373d11b666a
n-maido <nhi_maido@outlook.com>
2021-02-04 11:28:11 -0800

    add initial fellow info to JSON array

--- src/containers/Fellows/constants.js
+++ src/containers/Fellows/constants.js
@@ -1,6 +1,19 @@
 const fellows = [
     {
-
+        "name": "Devesh",
+        "bio": "Devesh is a full-stack developer & computer science graduate with a passion for product design.",
+        "github": "https://github.com/dev2919",
+        "linkedin": "https://www.linkedin.com/in/devesh-pawar-60304113b/",
+        "website": "https://www.devesh.design/",
+        "headshot": "https://drive.google.com/open?id=1MDD2RnGy4qqmv1f7AnMeYu3vQtdn6u_7",
+    },
+    {
+        "name": "Keshavaa",
+        "bio": "I love to learn new technologies at any time! I also love to play or watch basketball with my friends :)",
+        "github": "https://github.com/skeshavaa",
+        "linkedin": "https://www.linkedin.com/in/keshavaa/",
+        "website": "https://skeshavaa.live/",
+        "headshot": "https://drive.google.com/open?id=1TGxR4llNr8okrqMJauw_WqEmYaAM_CUq",
     }
 ]
 
