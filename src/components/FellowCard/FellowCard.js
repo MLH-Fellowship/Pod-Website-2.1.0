b8659063d11618871f147f524e24bb26bc0b9d69
Guna Shekar <gunashekherproddatoori@gmail.com>
2021-02-04 09:01:32 +0530

    fix eslint errors

--- src/components/FellowCard/FellowCard.js
+++ src/components/FellowCard/FellowCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './FellowCard.module.css';
 
 const FellowCard = () => {
-  return <div></div>;
+  return <div className={styles.container}></div>;
 };
 
 export default FellowCard;
