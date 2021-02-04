39a67f95f7097f3d287bb7ebbbfa881eb59f76bf
skeshavaa <s.keshavaa@gmail.com>
2021-02-04 18:12:04 -0500

    fixed some css and the heading for fellow section

--- src/components/FellowCard/FellowCard.js
+++ src/components/FellowCard/FellowCard.js
@@ -4,18 +4,21 @@ import styles from './FellowCard.module.css';
 import { MDBCol, MDBIcon } from "mdbreact";
 import PropTypes from 'prop-types';
 
-const FellowCard = (props) => {
+const FellowCard = ({fellow}) => {
+
+  const { name, bio } = fellow;
+
   return (
     <div className={styles.card}>
       <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
         <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg" className={styles.avatars}>
 
         </img>
-        <h5 className={styles.name}>{ props.name }</h5>
-        <p className={styles.position}>Team Member</p>
+        <h5 className={styles.name}>{ name }</h5>
+        {/* <p className={styles.position}>Team Member</p> */}
         <p className={styles.bio}>
-          I love XYZ and I have worked on ABC Projects.
-                </p>
+          {bio}  
+        </p>
         <ul className="list-unstyled mb-0">
           <a href="#!" className="p-2 fa-lg">
             <MDBIcon fab icon="facebook-f" className="blue-text" />
--- src/containers/Fellows/Fellows.js
+++ src/containers/Fellows/Fellows.js
@@ -2,33 +2,21 @@ import React from "react";
 import {  MDBRow, MDBCard, MDBCardBody } from "mdbreact";
 import FellowCard from "../../components/FellowCard/FellowCard";
 import styles from './Fellows.module.css'
+import fellows from './constants.js'
 
 const Fellows = () => {
-
-
-  const fellows = [
-    {
-      "name": "Keshavaa",
-    },
-    {
-      "name": "Nhi"
-    }
-  ]
 
 
   return (
     <MDBCard className="my-5 px-5 pb-5 text-center">
         <MDBCardBody>
-          <h2 className="h1-responsive font-weight-bold my-5">
+          <h2 className={styles.title}>
             Meet the Pod!
           </h2>
-          <p className="grey-text w-responsive mx-auto mb-5">
-            Divided by countries and culture, United by Technology ! :heart:
-          </p>
           <MDBRow className={styles.rows}>
             {fellows.map((fellow, index) => {
               return(
-                <FellowCard key={index} name={fellow.name}/>
+                <FellowCard key={index} fellow={fellow}/>
               )
             })}
           </MDBRow>
