7d30b44f854950586a583407a3dab2c5b2b9f25c
skeshavaa <s.keshavaa@gmail.com>
2021-02-04 12:26:51 -0500

    refactored fellow card component into reusable component

--- src/components/FellowCard/FellowCard.js
+++ src/components/FellowCard/FellowCard.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 import styles from './FellowCard.module.css';
+import { MDBCol, MDBIcon } from "mdbreact";
 
 const FellowCard = ({ name, bio, image }) => {
   return(
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        {/*  image goes here */}
-      </div>
-      <div className={styles.bioContainer}>
-        {/* bio goes here */}
-      </div>
-    </div>
+    <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg">
+              
+              </img>
+              <h5 className="font-weight-bold mt-4 mb-3">Yashika Sharma</h5>
+              <p className="text-uppercase blue-text">Team Leader</p>
+              <p className="grey-text">
+                I love XYZ and I have worked on ABC Projects.
+              </p>
+              <ul className="list-unstyled mb-0">
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="facebook-f" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="twitter" className="blue-text" />
+                </a>
+                <a href="#!" className="p-2 fa-lg">
+                  <MDBIcon fab icon="instagram" className="blue-text" />
+                </a>
+              </ul>
+            </MDBCol>
   )
 };
 
--- src/containers/Fellows/Fellows.js
+++ src/containers/Fellows/Fellows.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
+import {  MDBRow, MDBCard, MDBCardBody } from "mdbreact";
 
 const Fellows = () => {
   return (
@@ -12,103 +12,10 @@ const Fellows = () => {
             Divided by countries and culture, United by Technology ! :heart:
           </p>
           <MDBRow>
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg">
-              
-              </img>
-              <h5 className="font-weight-bold mt-4 mb-3">Yashika Sharma</h5>
-              <p className="text-uppercase blue-text">Team Leader</p>
-              <p className="grey-text">
-                I love XYZ and I have worked on ABC Projects.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="twitter" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="instagram" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
-
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(3).jpg">
-                
-      
-              </img>
-              <h5 className="font-weight-bold mt-4 mb-3">Robert Green</h5>
-              <p className="text-uppercase blue-text">Web Developer</p>
-              <p className="grey-text">
-              I love XYZ and I have worked on ABC Projects.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="instagram" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
-            </MDBRow>
-        </MDBCardBody>
-      </MDBCard>
-
-            /*<MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img>
-                tag="img"
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(30).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-                </img>
-              <h5 className="font-weight-bold mt-4 mb-3">Maria Smith</h5>
-              <p className="text-uppercase blue-text">Photographer</p>
-              <p className="grey-text">
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim est fugiat nulla id eu laborum.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="instagram" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="dribbble" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
-
-            <MDBCol lg="3" md="6" className="mb-lg-0 mb-5">
-              <img>
-                tag="img"
-                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg"
-                className="rounded-circle z-depth-1 img-fluid"
-                alt="Sample avatar"
-                </img>
-              <h5 className="font-weight-bold mt-4 mb-3">Tom Adams</h5>
-              <p className="text-uppercase blue-text">Backend Developer</p>
-              <p className="grey-text">
-                Perspiciatis repellendus ad odit consequuntur, eveniet earum
-                nisi qui consectetur totam officia voluptates perferendis
-                voluptatibus aut.
-              </p>
-              <ul className="list-unstyled mb-0">
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="facebook-f" className="blue-text" />
-                </a>
-                <a href="#!" className="p-2 fa-lg">
-                  <MDBIcon fab icon="github" className="blue-text" />
-                </a>
-              </ul>
-            </MDBCol>
+            {/* fellow cards called here */}
           </MDBRow>
         </MDBCardBody>
-      </MDBCard>*/
+      </MDBCard>
   );
 }
 
