a318e5f8d10e500ed7435ce4e7f4a2cfc1122a01
Abhishek-kumar09 <abhimait1909@gmail.com>
2021-02-04 15:04:37 +0530

    Donot merge

--- src/containers/Hero/Hero.js
+++ src/containers/Hero/Hero.js
@@ -3,7 +3,9 @@ import React from 'react';
 import styles from './Hero.module.css';
 
 const Hero = () => {
-  return <div className={styles.container}>HELLO THERE!</div>;
+  return <div className={styles.container}>HELLO THERE!
+  DOon't merge, just testing deploy preview here!
+  </div>;
 };
 
 export default Hero;
