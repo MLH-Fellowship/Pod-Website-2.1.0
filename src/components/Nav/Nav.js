02725784571cefb28dfa4f8eb0382181ba20365c
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-02-04 19:47:56 -0800

    Created the mobile navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,43 +1,67 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Nav.css';
-import {ReactComponent as Logo} from '../../assets/images/Logo.svg';
+import { ReactComponent as Logo } from '../../assets/images/Logo.svg';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-import {faSlack, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faSlack, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 // the limit of window.innerWidth that triggers the switch between NavDesktop and NavMobile.
 const WIDTH_LIMIT = 705;
-
-const Tab = ({icon, name, key}) => {
-    return (
-        <li key={key}>
-            <a>
-                <FontAwesomeIcon icon={icon} size="2x"/>
-                <p>{name}</p>
-            </a>
-        </li>
-    );
-}
 
 const tabsContent = [
     { icon: faHome, name: 'Home' },
     { icon: faSlack, name: 'Team' },
     { icon: faGithub, name: 'Projects' }
-];
+  ];
 
-const Nav = ({windowWidth}) => {
+const Tab = ({display}) => {
+  return (
+    tabsContent.map((content, index) => (
+        <li key={index} style={{display: display}}>
+            <a>
+                <FontAwesomeIcon icon={content.icon} size="2x" />
+                <p>{content.name}</p>
+            </a>
+        </li>
+      ))
+  );
+};
+
+const MobileMenuBtn = ({isMobileMenuOpen, setMobileMenu}) => {
+    const toggleMobileMenu = () => setMobileMenu(!isMobileMenuOpen); 
 
     return (
-        <div id='nav'>
-            <div className='left'>
-                <Logo style={{width: "20%", height: "100%"}}/>
-            </div>
-            <ul className='right'>
-                {tabsContent.map((content, index) => <Tab {...content} key={index}/>)}
-            </ul>
+        <div className={'mobile-menu-btn' + (isMobileMenuOpen ? " change" : "")} onClick={toggleMobileMenu}>
+            <div className='bar1'></div>
+            <div className='bar2'></div>
+            <div className='bar3'></div>
         </div>
     );
+};
+
+const Nav = ({ windowWidth }) => {
+  const [isMobileMenuOpen, setMobileMenu] = useState(false);
+
+  useEffect(() => setMobileMenu(false), [windowWidth]);
+
+  return (
+    <div id="nav">
+      <div className="left-header">
+        <Logo style={{ width: '20%', height: '100%' }} />
+      </div>
+      {windowWidth >= WIDTH_LIMIT ? (
+        <ul className="right-header-desktop">
+          <Tab/>
+        </ul>
+      ) : (
+        <ul className="right-header-mobile">
+          <MobileMenuBtn isMobileMenuOpen={isMobileMenuOpen} setMobileMenu={setMobileMenu}/>
+          <Tab display={isMobileMenuOpen ? 'initial' : 'none'}/>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Nav;
